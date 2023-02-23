3568b715227f3088261edae387048e21147c17c6
Paulinavegac <paulina.vegac@gmail.com>
2023-02-23 16:44:13 -0300

    test corriendo

--- src/index.js
+++ src/index.js
@@ -1,33 +1,55 @@
 import validator from './validator.js';
-console.log(validator);
-console.log(validator);
+
+//Elementos del DOM todos son const
+const cardNumber = document.getElementById('inputCard');
+const btnValidation = document.getElementById('btnValidation');
+
+const viewMaskifyCard = document.getElementById('viewMaskifyCard');
+const validCard = document.getElementById('validCard');
+const maskifyNumber = document.getElementById('maskifyNumber')
+const validCardSection = document.getElementById('validCardSection')
 
 
 
+// Funciones que interactúan con el DOM
+function emptyInput() {
+  const emptyCard = cardNumber.value.length === 0;
+  if (emptyCard) {
+    alert('Ingresa un número de tarjeta nuevamente')
+  }
+  return emptyCard
+}
+function mostrarValidar() {
+  const cardNumber = document.getElementById('inputCard').value
+  const validar = validator.isValid(cardNumber);
+  if (validar) {
+    validCard.innerHTML = 'Tarjeta Válida'
 
-function validar() {
-    let cardNumber = document.getElementById("cardnumber").value;
-    console.log('cardNumber=')
-    console.log(cardNumber)
-    const arrayCard = cardNumber.split('');
-    console.log('arrayCard=')
-    console.log(arrayCard)
-    const reverseCard = arrayCard.reverse();
-    console.log('reverseCard='+reverseCard)
-    console.log('cardNumber='+cardNumber)
-    let pairs = [];
-    for (let i = 1; i < reverseCard.length; i = i + 2) {
-        pairs.push([i])
-    }
-    console.log('pairs='+pairs)
-    alert("Presionaste validar")
+  } else {
+    validCard.innerHTML = 'Tarjeta Inválida. Ingresa nuevamente.'
+  }
+}
+function mostrarTarjeta() {
+  const cardNumber = document.getElementById('inputCard').value
+  maskifyNumber.innerHTML = validator.maskify(cardNumber);
+}
+// funcion ocultar el input y aparecer validación tarjeta de crédito
+function ocultarInput() {
+  validCardSection.style.display = 'block'
+  viewMaskifyCard.style.display = 'block'
+
 }
 
-document.getElementById("validate").addEventListener("click", validar)
+// Eventos del DOM
+
+btnValidation.addEventListener('click', clickbtn);
+function clickbtn() {
+  const isEmpty = emptyInput();
+  if (!isEmpty) {
+    mostrarValidar();
+    mostrarTarjeta(); 
+    ocultarInput();
+  }
 
 
-
-
-
-
-
+}
